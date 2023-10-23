f98c08994b49daab0ee91550317b750dba9020f5
UnknownProgrammer-Ag <trabajospracticosescuela@hotmail.com>
2023-10-23 16:56:33 -0300

    Modificacion de procesos_csv y test en main       Procesos_csv ahora colocara la información del csv a diferentes instancias de la clase Proceso, que se almacenara en una lista de objetos

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from procesos_csv import csv_opener
+from procesos_csv import Proceso
 
 path = input('Ingrese la dirección del .csv\n')
-data = csv_opener(path)
-# len(data)-> La cantidad de procesos a ingresar
+arch = csv_opener(path)
+# print(len(arch))  -> La cantidad de procesos a ingresar
+
+# Con este código puede comprobar que se copiaron correctamente los valores del .csv.
+for proc in arch:
+    print("Proceso ", proc.id, "Tamaño ", proc.tam,
+          "TA ", proc.arribo, "TI ", proc.irrup)
--- procesos_csv.py
+++ procesos_csv.py
@@ -1,15 +1,20 @@
 import csv
 
 
+class Proceso:
+    def __init__(self, id, tam, tA, tI):
+        self.id = id
+        self.tam = tam
+        self.arribo = tA
+        self.irrup = tI
+
+
 def csv_opener(direc):
     file = open(direc, newline='')
     reader = csv.reader(file)
     next(reader)
-    data = []
+    arch_procesos = []
     for row in reader:
-        id = int(row[0])
-        tam = int(row[1])
-        tA = int(row[2])
-        tI = int(row[3])
-        data.append([id, tam, tA, tI])
-    return data
+        proc = Proceso(int(row[0]), int(row[1]), int(row[2]), int(row[3]))
+        arch_procesos.append(proc)
+    return arch_procesos
