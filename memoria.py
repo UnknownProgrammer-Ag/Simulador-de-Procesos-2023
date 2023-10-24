d5b81ceb50565a289ddd11a08d7d591763f5c971
UnknownProgrammer-Ag <trabajospracticosescuela@hotmail.com>
2023-10-24 02:14:17 -0300

    Se agrego las particiones, estado:a pulir

--- memoria.py
+++ memoria.py
@@ -17,3 +17,13 @@ class Particiones:
         self.tam = self.tam + self.frag
         self.frag = 0
         self.ocupado = 0
+
+memoria=[]
+part0=Particiones(0,0,100,0,1)
+part1=Particiones(1,100000,250,0,0)
+part2=Particiones(2,350000,120,0,0)
+part3=Particiones(3,470000,60,0,0)
+memoria.append(part0)
+memoria.append(part1)
+memoria.append(part2)
+memoria.append(part3)
