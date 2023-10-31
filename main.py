831ed9dc16dae2c8cd144876d9ca55304103f6a9
UnknownProgrammer-Ag <trabajospracticosescuela@hotmail.com>
2023-10-31 18:34:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ from terminaltables import AsciiTable
 import tkinter as tk
 from tkinter import filedialog
 from time import sleep
-from threading import Event
+from threading import Event, Thread
 
 root = tk.Tk()
 root.withdraw()
-def simulador():
+pausa_simulador = Event()
+
+estado_Simulador = {"ejecutando": True, "salida": ""}
 
 
+# Principal
 print("Bienvenido a Simulador ROUND ROBIN de Procesos")
 print("Ahora, para iniciar el simulador ingrese el archivo CSV que contendra los procesos")
 sleep(2)
+
 path = filedialog.askopenfilename(title="Seleccionar Archivo CSV", filetypes=[
                                   ("Archivo csv", "*.csv"), ("Todos los archivos", "*.*")])
 arch = csv_opener(path)
+
+# Llamada al Simulador
 if arch != None:
     tiempo_total = 0
+    print(tiempo_total)
+
 else:
     print("El archivo csv estaba vacío o fallo en cargar")
