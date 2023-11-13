150d19e44bfc637204bd79db94cce48af4b97298
UnknownProgrammer-Ag <trabajospracticosescuela@hotmail.com>
2023-11-13 16:26:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,22 @@ def simulador(arch):
                 nuevos.append(temp)
             else:
                 arch.appendleft(temp)
-                list(nuevos)
-                break
+                if tiempo_total == 0:
+                    sal.estado_procesador()
+                else:
+                    sal.estado_procesador(CPU.proceso.id, CPU.proceso.estado)
+
+                sal.tabla_memoria()
+                sal.mostrar_listos()
+
+                while True:
+                    user = input("Ingrese Enter para Continuar...")
+                    if user == "":
+                        print("Continuando...")
+                        sleep(1)
+                        break
+                    else:
+                        print("Debe ser enter para continuar...")
         # Cargar Listos si la memoria no esta ocupada -> Mantener Multiprogramación de 5
         while contProc < 5:
             if nuevos:
@@ -40,40 +54,54 @@ def simulador(arch):
             else:
                 list(listos)
                 break
-
         # Cargar Particiones
         while (memoria_principal.ocupadas != 3):
             if listos:
                 temp = listos.popleft()
+
                 if not memoria_principal.best_Fit(temp):
                     listos.append(temp)
                     # Quiere decir que no hay particion en este momento que aloje al tamaño, pero si hay disponible
         list(listos)
-
         # Cargar Procesador
         if not CPU.ocupado:
             # Simple Variable para obtener menor ID (presuntamente el proceso que debe ingresar)
             min = 11  # SE sabe que hay un maximo de 10 procesos
+
             for part in memoria_principal:
                 if part.proceso.id < min:
                     min = part.proceso.id
                     temp = part.proceso
-
             CPU.cargar(temp)
 
         CPU.procesar()
         tiempo_total += 1
+
         if CPU.proceso.irrup == 0:
             print(f"Proceso {CPU.proceso.id} termino...")
+
             item = OutPut(CPU.proceso.id, CPU.proceso.arribo,
                           CPU.proceso.resgirrup, tiempo_total)
             lista_sal.append(item)
             for part in memoria_principal:
+
                 if part.proceso.irrup == CPU.proceso.id:
                     part.descargar()
             CPU.reiniciar_q()
+
             sal.estado_procesador()
             sal.tabla_memoria()
+            sal.mostrar_listos()
+            while True:
+
+                user = input("Ingrese Enter para Continuar...")
+                if user == "":
+                    print("Continuando...")
+
+                    sleep(1)
+                    break
+                else:
+                    print("Debe ser enter para continuar...")
             contProc -= 1
         else:
             if CPU.quantum == 0:
